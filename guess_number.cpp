011dd60973c6eb5e80c14455ce1041212ebbe85d
Bipin karki <karkeybipin@gmail.com>
2024-09-23 22:13:28 +0545

    Updated new codes and resolved some of the errors

--- guess_number.cpp
+++ guess_number.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <cstdlib>  // for rand() and srand()
-#include <ctime>    // for time()
+#include <cstdlib> // for rand() and srand()
+#include <ctime>   // for time()
 
-int main() {
-    // Initialize random seed based on the current time
+int main()
+{
     std::srand(std::time(0));
-
-    // Generate a random number between 1 and 100
     int numberToGuess = std::rand() % 100 + 1;
     int playerGuess;
     int numberOfAttempts = 0;
@@ -17,27 +15,42 @@ int main() {
     std::cout << "You have " << maxAttempts << " attempts to guess it.\n";
 
     // Game loop
-    while (numberOfAttempts < maxAttempts) {
+    while (numberOfAttempts < maxAttempts)
+    {
         std::cout << "\nEnter your guess: ";
         std::cin >> playerGuess;
 
         numberOfAttempts++;
 
-        if (playerGuess == numberToGuess) {
-            std::cout << "Congratulations! You guessed the number in " 
+        if (playerGuess == numberToGuess)
+        {
+            std::cout << "Congratulations! You guessed the number in "
                       << numberOfAttempts << " attempts.\n";
             break;
-        } else if (playerGuess > numberToGuess) {
+        }
+        else if (playerGuess > numberToGuess)
+        {
             std::cout << "Too high! Try again.\n";
-        } else {
+        }
+        else
+        {
             std::cout << "Too low! Try again.\n";
         }
 
-        if (numberOfAttempts == maxAttempts) {
-            std::cout << "\nYou've used all your attempts. The number was: " 
+        if (numberOfAttempts == maxAttempts)
+        {
+            std::cout << "\nYou've used all your attempts. The number was: "
                       << numberToGuess << ". Better luck next time!\n";
         }
     }
 
     return 0;
 }
+
+
+// if (numberOfAttempts == maxAttempts)
+//         {
+//             std::cout << "\nYou've used all your attempts. The number was: "
+//                       << numberToGuess << ". Better luck next time!\n";
+//         }
+//     }
