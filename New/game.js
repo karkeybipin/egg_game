aae55df9f8ef904a642547d6c57861e8f94a8e4d
Bipin karki <karkeybipin@gmail.com>
2024-09-29 17:29:18 +0545

    Some changes made for the Post & Add page || Game related codes in New folder

--- New/game.js
+++ New/game.js
@@ -76,3 +76,136 @@ function drawPlayer() {
     ctx.fillStyle = 'green';
     ctx.fillRect(player.x, player.y, player.width, player.height);
 }
+
+
+let canvas, ctx;
+let player = { x: 375, y: 500, width: 50, height: 50, speed: 5 };
+let bullets = [];
+let enemies = [];
+let keys = {};
+let score = 0;
+let gameLoop;
+
+function initGame() {
+    canvas = document.getElementById('gameCanvas');
+    ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 600;
+
+    document.addEventListener('keydown', keyDownHandler);
+    document.addEventListener('keyup', keyUpHandler);
+
+    gameLoop = setInterval(updateGame, 1000 / 60);
+    setInterval(spawnEnemy, 2000);
+}
+
+function keyDownHandler(e) {
+    keys[e.code] = true;
+}
+
+function keyUpHandler(e) {
+    keys[e.code] = false;
+}
+
+function shoot() {
+    bullets.push({
+        x: player.x + player.width / 2 - 2.5,
+        y: player.y,
+        width: 5,
+        height: 10,
+        speed: 7
+    });
+}
+
+function updateGame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (keys['ArrowLeft'] || keys['KeyA']) {
+        player.x -= player.speed;
+        if (player.x < 0) player.x = 0;
+    }
+    if (keys['ArrowRight'] || keys['KeyD']) {
+        player.x += player.speed;
+        if (player.x + player.width > canvas.width) player.x = canvas.width - player.width;
+    }
+    if (keys['Space']) {
+        shoot();
+    }
+
+    for (let i = 0; i < bullets.length; i++) {
+        bullets[i].y -= bullets[i].speed;
+        if (bullets[i].y < 0) {
+            bullets.splice(i, 1);
+            i--;
+        }
+    }
+
+    for (let i = 0; i < enemies.length; i++) {
+        enemies[i].y += enemies[i].speed;
+        if (enemies[i].y > canvas.height) {
+            gameOver();
+        }
+
+        for (let j = 0; j < bullets.length; j++) {
+            if (collisionDetected(bullets[j], enemies[i])) {
+                bullets.splice(j, 1);
+                enemies.splice(i, 1);
+                score += 10;
+                i--;
+                break;
+            }
+        }
+    }
+
+    drawPlayer();
+    
+    bullets.forEach(bullet => {
+        ctx.fillStyle = 'yellow';
+        ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+    });
+
+    enemies.forEach(enemy => {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+    });
+
+    drawScore();
+}
+
+function drawPlayer() {
+    ctx.fillStyle = 'green';
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+}
+
+function drawScore() {
+    ctx.fillStyle = 'white';
+    ctx.font = '20px Arial';
+    ctx.fillText('Score: ' + score, 10, 30);
+}
+
+function collisionDetected(bullet, enemy) {
+    return bullet.x < enemy.x + enemy.width &&
+        bullet.x + bullet.width > enemy.x &&
+        bullet.y < enemy.y + enemy.height &&
+        bullet.y + bullet.height > enemy.y;
+}
+
+function spawnEnemy() {
+    const enemy = {
+        x: Math.random() * (canvas.width - 50),
+        y: -50,
+        width: 50,
+        height: 50,
+        speed: 2 + Math.random() * 3
+    };
+    enemies.push(enemy);
+}
+
+function gameOver() {
+    clearInterval(gameLoop);
+    ctx.fillStyle = 'red';
+    ctx.font = '50px Arial';
+    ctx.fillText('GAME OVER', canvas.width / 2 - 150, canvas.height / 2);
+}
+
+initGame();
