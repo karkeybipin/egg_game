9c85a60325909077de1dee6f2b86f23df1444266
Bipin karki <karkeybipin@gmail.com>
2024-10-11 22:10:41 +0545

    code solved

--- detailnetService.py
+++ detailnetService.py
@@ -3,7 +3,6 @@ import psutil
 def get_network_stats():
     net_io = psutil.net_io_counters()
     print(f"Bytes Sent: {net_io.bytes_sent / (1024**2):.2f} MB")
-        #   (1024**2):.2f} MB")
     print(f"Bytes Received: {net_io.bytes_recv / (1024**2):.2f} MB")
     connections = psutil.net_connections(kind='inet')
     print("\nActive Network Connections:")
