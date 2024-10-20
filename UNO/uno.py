441789aa92a40db838bb8c0cccf9ac2f8e479ec4
Bipin karki <karkeybipin@gmail.com>
2024-10-20 23:25:06 +0545

    code

--- UNO/uno.py
+++ UNO/uno.py
@@ -209,8 +209,6 @@ pygame.init()
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("UNO Game")
-
-# Colors
 GREEN_BACKGROUND = (0, 128, 0)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
